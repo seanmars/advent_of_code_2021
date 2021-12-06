3f62cddf73857bd7b6f4eef272692d026a5c5091
seanmars <mars0222@gmail.com>
2021-12-07 02:16:47 +0800

    Update day 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Core;
 
@@ -7,6 +11,80 @@ namespace Day02
 {
     class Program
     {
+        public static class CommandName
+        {
+            public const string Forward = "forward";
+            public const string Down = "down";
+            public const string Up = "up";
+        }
+
+        public enum CommandType
+        {
+            Forward,
+            Down,
+            Up
+        }
+
+        public class Position
+        {
+            public int Horizontal { get; set; }
+            public int Aim { get; set; }
+            public int Depth { get; set; }
+            public int NoSenseDepth => Horizontal * Aim;
+            public int FinalDepth => Horizontal * Depth;
+        }
+
+        public struct Command
+        {
+            public CommandType Type { get; set; }
+            public int Value { get; set; }
+        }
+
+        static Position Get_1_1_Answer(IEnumerable<Command> input)
+        {
+            var pos = new Position();
+            foreach (var command in input)
+            {
+                switch (command.Type)
+                {
+                    case CommandType.Forward:
+                        pos.Horizontal += command.Value;
+                        break;
+                    case CommandType.Down:
+                        pos.Aim += command.Value;
+                        break;
+                    case CommandType.Up:
+                        pos.Aim -= command.Value;
+                        break;
+                }
+            }
+
+            return pos;
+        }
+
+        static Position Get_1_2_Answer(IEnumerable<Command> input)
+        {
+            var pos = new Position();
+            foreach (var command in input)
+            {
+                switch (command.Type)
+                {
+                    case CommandType.Forward:
+                        pos.Horizontal += command.Value;
+                        pos.Depth += command.Value * pos.Aim;
+                        break;
+                    case CommandType.Down:
+                        pos.Aim += command.Value;
+                        break;
+                    case CommandType.Up:
+                        pos.Aim -= command.Value;
+                        break;
+                }
+            }
+
+            return pos;
+        }
+
         static async Task Main(string[] args)
         {
             try
@@ -15,7 +93,23 @@ namespace Day02
 
                 var lines = await inputManager.GetInput();
 
-                Console.WriteLine();
+                var commands = lines.Select(x =>
+                {
+                    var tmp = x.Split(' ');
+                    return tmp[0] switch
+                    {
+                        CommandName.Forward => new Command { Type = CommandType.Forward, Value = Convert.ToInt32(tmp[1]) },
+                        CommandName.Down => new Command { Type = CommandType.Down, Value = Convert.ToInt32(tmp[1]) },
+                        CommandName.Up => new Command { Type = CommandType.Up, Value = Convert.ToInt32(tmp[1]) },
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+                }).ToList();
+
+                var pos = Get_1_1_Answer(commands);
+                Console.WriteLine(JsonSerializer.Serialize(pos));
+
+                pos = Get_1_2_Answer(commands);
+                Console.WriteLine(JsonSerializer.Serialize(pos));
             }
             catch (HttpRequestException e)
             {
