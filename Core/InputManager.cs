18f328b44622b41453b4b44fe3f97b7972bb591a
Mars <mars0222@gmail.com>
2022-12-05 11:50:59 +0800

    Update InputManager.cs

--- Core/InputManager.cs
+++ Core/InputManager.cs
@@ -19,7 +19,7 @@ namespace Core
         public async Task<List<string>> GetInput()
         {
             _http.DefaultRequestHeaders.Add("Cookie",
-                "session=53616c7465645f5f5f44b3f0bbefb6f70eb1601fbc0138659399f063586e724ba1c691e02d8a012cb1e426bbe2f8464d");
+                "session=");
             var response = await _http.GetAsync($"https://adventofcode.com/2021/day/{_day}/input");
             response.EnsureSuccessStatusCode();
             var responseBody = await response.Content.ReadAsStringAsync();
